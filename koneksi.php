4c5a7977e51a25ead86026d445c558201c72f873
jiankezia <jian.kezia@gmail.com>
2024-02-15 01:08:45 +0800

    baru

--- koneksi.php
+++ koneksi.php
@@ -2,7 +2,7 @@
 $host       = "localhost";
 $user       = "root";
 $pass       = "";
-$db         = "researcher_db";
+$db         = "researcherdb";
 
 $koneksi = mysqli_connect($host,$user,$pass,$db);
 if(!$koneksi){
